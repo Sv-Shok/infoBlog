3c1b0831ce21f765f91229e86aecb7350e688317
Roman <svshok1@gmail.com>
2021-08-13 14:47:29 +0300

    create comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :bigint           not null, primary key
+#  body       :text
+#  username   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  post_id    :bigint           not null
+#
+# Indexes
+#
+#  index_comments_on_post_id  (post_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (post_id => posts.id)
+#
 class Comment < ApplicationRecord
   belongs_to :post
+  validates :username, presence: true, length: { minimum: 3 }
+  validates :body, presence: true, length: { minimum: 10 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  body       :text
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5 }
