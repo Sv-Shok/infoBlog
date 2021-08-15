e6afb421fcee569938115e8660c752262df9b2c6
Roman <svshok1@gmail.com>
2021-08-15 18:02:50 +0300

    connect between Posts and Users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   # http_basic_authenticate_with name: 'admin', password: '1234', except: %i[index show]
-
+  before_action :authenticate_user!, except: %i[index show]
   def index
     @post = Post.all
   end
@@ -19,6 +19,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    @post.user_id = current_user.id
     if @post.save
       redirect_to @post
     else
@@ -45,6 +46,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :body)
+    params.require(:post).permit(:title, :body, :user_id)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,8 +7,14 @@
 #  title      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer
+#
+# Indexes
+#
+#  index_posts_on_user_id  (user_id)
 #
 class Post < ApplicationRecord
+  belongs_to :user
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5 }
 end
